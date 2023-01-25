ad7aedb1ca55554c4ad4c3ec51de4544aad560c8
a <royamit1998@gmail.com>
2023-01-25 18:10:00 +0200

    delete unused functions in server

--- server.cpp
+++ server.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
 #include <sys/socket.h>
-#include <stdio.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
 #include <string.h>
-#include <bits/stdc++.h>
 #include "validations.h"
 #include "vectorData.h"
 #include <thread>
-#include <unistd.h>
 #include "CLI.h"
 #include "connectionUtil.h"
 
@@ -49,169 +44,10 @@ bool check_valid_port(char *port) {
 }
 
 /**
- * converts character array to string and returns it
- * @param a the char array
- * @param size size of the array
- * @return the string
- */
-string convertToString(char *a, int size) {
-    int i;
-    string s = "";
-    for (i = 0; i < size; i++) {
-        s = s + a[i];
-    }
-    return s;
-}
-
-/**
- * This function creates a new vectorData that holds the user input parameters
- * @param userInput - string of the following format: vector, distance, k
- * @return a new vectorData object
- */
-vectorData createVecData(string userInput, int read_bytes) {
-    bool flag = true;
-    int last_index = userInput.find_last_of(' ');
-    int first_index;
-    string distance;
-    // splitting the k parameter from user input
-    string k = userInput.substr(last_index + 1, read_bytes - last_index);
-    for (int i = last_index - 1; i >= 0; --i) {
-        // splitting the distance parameter from user input
-        if (userInput[i] == ' ' && flag) {
-            first_index = i;
-            // splitting the vector parameter from the user input
-            distance = userInput.substr(i + 1, last_index - (first_index + 1));
-            // check if the distance parameter is valid
-            flag = false;
-        }
-    }
-    // splitting the vector parameter from the user input
-    string vec = userInput.substr(0, first_index);
-    // check if the vector is valid
-    vectorData v = vectorData(vec, distance, stoi(k));
-    return v;
-}
-
-/**
- * classifying the vector according to the arguments the user gave us
- * @param buffer pointer to the start of array with the the information from the user
- * @param file_name path to the file of the vectors
+ *
+ * @param port
  * @return
  */
-string classify(char *buffer, string file_name, int read_bytes) {
-//    string userInput = convertToString(buffer, read_bytes);
-//    if (!check_valid_user_input(userInput, read_bytes)) {
-//        return ERROR;
-//    }
-//    // getting the vectors from the file
-//    vector<classifiedVector> allClassVec = fileToVec(file_name);
-//    // separating the user's input to the different values
-//    vectorData vecData = createVecData(userInput, read_bytes);
-//    // case - k is bigger than the amount of vectors in the file
-//    if (allClassVec.size() < vecData.getK()) {
-//        return ERROR;
-//    }
-//    // getting the classification
-//    string classification = getClassification(allClassVec, vecData.getDistance(),
-//                                              vecData.getK(), vecData.getVec());
-//    return classification;
-}
-
-/**
- * connecting to client
- * @param sock the socket that the client going to be connected to
- * @return the client socket
- */
-int connectClient(int sock) {
-    struct sockaddr_in client_sin;
-    unsigned int addr_len = sizeof(client_sin);
-    int client_sock = accept(sock, (struct sockaddr *) &client_sin, &addr_len);
-
-    if (client_sock < 0) {
-        cout << "error accepting client" << endl;
-        exit(1);
-    }
-    return client_sock;
-}
-
-/**
- * accepting the vector from the client and returning the classification
- * @param port the port
- * @param file the file is going to be used for the classification
- */
-void acceptVector(int port, string file) {
-    bool flag = false;
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        cout << "error creating socket" << endl;
-        exit(1);
-    }
-    struct sockaddr_in sin;
-    memset(&sin, 0, sizeof(sin));
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = INADDR_ANY;
-    sin.sin_port = htons(port);
-    //bind
-    if (bind(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
-        cout << "error binding socket" << endl;
-        exit(1);
-    }
-    if (listen(sock, 5) < 0) {
-        cout << "error listening to a socket" << endl;
-        exit(1);
-    }
-    while (true) {
-        int client_sock = connectClient(sock);
-        while (true) {
-            char buffer[BUFFERSIZE] = "\0";
-            int client_sock = connectClient(sock);
-            int expected_data_len = sizeof(buffer);
-            int read_bytes = recv(client_sock, buffer, expected_data_len, 0);
-
-            if (read_bytes == 0) {
-                cout << "connection is closed" << endl;
-                break;
-            } else if (read_bytes < 0) {
-                cout << "problem with connection" << endl;
-                break;
-            } else {
-                if (strcmp(buffer, "-1") == 0) {
-                    break;
-                }
-            }
-
-            // classify
-            string tmpClassification = classify(buffer, file, read_bytes);
-            // converting to buffer
-            strcpy(buffer, tmpClassification.c_str());
-            // sending the classification to the client
-            int sent_bytes = send(client_sock, buffer, tmpClassification.length() + 1, 0);
-            if (sent_bytes < 0) {
-                cout << "error sending to client" << endl;
-                break;
-            }
-        }
-    }
-}
-//bool acceptFromClient(int sock,char* &buffer) {
-//    int client_sock = connectClient(sock);
-//        int expected_data_len = sizeof(buffer);
-//        int read_bytes = recv(client_sock, buffer, expected_data_len, 0);
-//
-//        if (read_bytes == 0) {
-//            cout << "connection is closed" << endl;
-//            return false;
-//        } else if (read_bytes < 0) {
-//            cout << "problem with connection" << endl;
-//            return false;
-//        } else {
-//            if (strcmp(buffer, "-1") == 0) {
-//                return false;
-//            }
-//        }
-//
-//}
-
 int makeConnection(int port) {
     bool flag = false;
     int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -236,31 +72,30 @@ int makeConnection(int port) {
     return sock;
 }
 
+/**
+ *
+ * @param client_sock
+ */
 void handleThread(int client_sock) {
     SocketIO newSock;
     newSock.setClientSock(client_sock);
     CLI currentCli(&newSock);
     currentCli.start();
-    int x=0;
+    int x = 0;
 
 }
 
+/**
+ *
+ * @param sock
+ */
 void handleClients(int sock) {
     vector<thread> threadsVector;
     while (true) {
         int client_sock = connectToClient(sock);
         threadsVector.emplace_back(handleThread, client_sock);
         threadsVector.at(threadsVector.size() - 1).detach();
-//        for (int i = 0; i < threadsVector.size(); i++) {
-//            threadsVector.at(i).join();
-//        }
     }
-}
-
-void handleInoutThread(int id) {
-    StandardIO newS;
-    CLI currentCli(&newS);
-    currentCli.start();
 }
 
 /**
@@ -275,8 +110,5 @@ int main(int argc, char *argv[]) {
     const int server_port = stoi(argv[1]);
     int sock = makeConnection(server_port);
     handleClients(sock);
-    //thread t(handleThread,1);
-    //acceptVector(server_port, file_name);
-    //handleInoutThread(1);
     return 0;
 }
